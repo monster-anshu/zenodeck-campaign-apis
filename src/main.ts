713447e5350b2625168c2499998fb5c20abe3fbc
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-01-11 15:43:30 +0530

    migrate to fasttify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    })
+  );
 
   app.setGlobalPrefix('/api/v1/campaign');
 
@@ -16,6 +25,6 @@ async function bootstrap() {
     useGlobalPrefix: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
