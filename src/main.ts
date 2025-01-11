74f901bb1735d634b439a9a214192dd003e41f36
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-01-11 14:12:39 +0530

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api/v1/campaign');
+
+  const config = new DocumentBuilder()
+    .setTitle('Zenodeck Campaign Apis')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    useGlobalPrefix: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
