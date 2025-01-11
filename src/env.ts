38194b0bd145616364a81c471409f04935dcdcf2
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-01-11 18:09:07 +0530

    feat: add mongoose

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,5 @@
 import 'dotenv/config';
 
 export const PORT = +(process.env.PORT || 3000);
+export const MONGO_DEFAULT_URI = process.env.MONGO_DEFAULT_URI as string;
+export const MONGO_COMMON_URI = process.env.MONGO_COMMON_URI as string;
---
+++ src/mongo/connections.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { MONGO_COMMON_URI, MONGO_DEFAULT_URI } from '~/env';
+
+const isProd = process.env.NODE_ENV !== 'development';
+
+if (!isProd) {
+  mongoose.set('debug', true);
+}
+
+mongoose.set('runValidators', true);
+mongoose.set('strict', true);
+mongoose.set('autoIndex', !isProd);
+
+export const MONGO_CONNECTION = {
+  DEFAULT: mongoose.createConnection(MONGO_DEFAULT_URI),
+  COMMON: mongoose.createConnection(MONGO_COMMON_URI),
+};
+
+MONGO_CONNECTION.DEFAULT.on('connected', function () {
+  console.log('Mongoose: default connection opened');
+});
+
+MONGO_CONNECTION.COMMON.on('connected', function () {
+  console.log('Mongoose: common connection opened');
+});
