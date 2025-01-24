038ee377cab6ecde42d12d1b50d359a1fe0b0c6b
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-01-24 17:05:20 +0530

    fix: missing guard from credential controller

--- src/agent/agent.module.ts
+++ src/agent/agent.module.ts
@@ -1,5 +1,4 @@
 import { Global, Module } from '@nestjs/common';
-import { CampaignAppModule } from '~/campaign-app/campaign-app.module';
 import { RoleModule } from '~/role/role.module';
 import { AgentController } from './agent.controller';
 import { AgentService } from './agent.service';
@@ -9,6 +8,6 @@ import { AgentService } from './agent.service';
   controllers: [AgentController],
   providers: [AgentService],
   exports: [AgentService],
-  imports: [RoleModule, CampaignAppModule],
+  imports: [RoleModule],
 })
 export class AgentModule {}
