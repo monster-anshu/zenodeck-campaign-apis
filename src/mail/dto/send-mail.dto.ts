02f536d93fc5edf5931a648960500dd33fd0e402
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-01-28 20:47:01 +0530

    feat: add html render

--- src/mail/dto/send-mail.dto.ts
+++ src/mail/dto/send-mail.dto.ts
@@ -1,11 +1,12 @@
 import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 import { MongoIdZod } from '~/lib/zod';
+import { template } from '../t';
 
 const SendMailZod = z.object({
   credentialId: MongoIdZod,
   subject: z.string().nonempty(),
-  body: z.string().nonempty(),
+  projectData: z.string().catch(template),
   from: z.string().email(),
   to: z.string().email(),
 });
