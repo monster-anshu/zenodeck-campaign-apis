27eae194c494495eabfd0ce95829a78181c2459d
monster-anshu <hgunwant2312@gmail.com>
2025-02-04 22:27:15 +0530

    feat: add history to dashboard api

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AgentGuard } from '~/agent/agent.guard';
+import { GetSession } from '~/session/session.decorator';
+import { DashboardService } from './dashboard.service';
 
+@UseGuards(AgentGuard)
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+  constructor(private readonly dashboardService: DashboardService) {}
+
+  @Get()
+  async get(@GetSession('appId') appId: string) {
+    const res = await this.dashboardService.get(appId);
+    return {
+      ...res,
+      isSuccess: true,
+    };
+  }
+}
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,76 @@
 import { Injectable } from '@nestjs/common';
+import { Types } from 'mongoose';
+import dayjs from '~/lib/dayjs';
+import { EmailHistoryModel } from '~/mongo/campaign';
 
 @Injectable()
-export class DashboardService {}
+export class DashboardService {
+  async get(appId: string) {
+    const [history] = await Promise.all([this.history(appId)]);
+    return { history };
+  }
+
+  async history(appId: string) {
+    const last7Days = new Date();
+    last7Days.setDate(last7Days.getDate() - 7);
+
+    const last7DaysPromise = EmailHistoryModel.aggregate<{
+      _id: string;
+      count: number;
+    }>([
+      {
+        $match: {
+          appId: new Types.ObjectId(appId),
+          createdAt: { $gte: last7Days }, // Get emails from the last 7 days
+        },
+      },
+      {
+        $addFields: {
+          date: {
+            $dateToString: { format: '%Y-%m-%d', date: '$createdAt' },
+          },
+        },
+      },
+      {
+        $group: {
+          _id: '$date',
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { _id: 1 }, // Sort by date
+      },
+    ]);
+
+    const oneDayMs = 24 * 60 * 60 * 1000;
+    const last24Hours = new Date(Date.now() - oneDayMs);
+
+    // Get the last 24 hours' email count
+    const last24HoursCountPromise = EmailHistoryModel.countDocuments({
+      appId: new Types.ObjectId(appId),
+      createdAt: { $gte: last24Hours },
+    });
+
+    const [stats, count] = await Promise.all([
+      last7DaysPromise,
+      last24HoursCountPromise,
+    ]);
+
+    const dateMap = new Map(stats.map(({ _id, count }) => [_id, count]));
+    const filledStats: { date: string; count: number }[] = [];
+
+    let date = dayjs();
+    for (let i = 0; i < 7; i++) {
+      const formattedDate = date.format('YYYY-MM-DD');
+
+      filledStats.push({
+        date: formattedDate,
+        count: dateMap.get(formattedDate) ?? 0,
+      });
+
+      date = date.add(-oneDayMs, 'millisecond');
+    }
+
+    return { stats: filledStats, count };
+  }
+}
