be15556430012162ab1238a10924068da86384af
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-02-11 21:04:53 +0530

    feat: add update lead

--- src/lead/lead.controller.ts
+++ src/lead/lead.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch, UseGuards } from '@nestjs/common';
+import { AgentGuard } from '~/agent/agent.guard';
+import { GetSession } from '~/session/session.decorator';
+import { UpdateLeadDto } from './dto/update-lead.dto';
+import { LeadService } from './lead.service';
 
+@UseGuards(AgentGuard)
 @Controller('lead')
-export class LeadController {}
+export class LeadController {
+  constructor(private readonly leadService: LeadService) {}
+
+  @Patch()
+  async update(
+    @GetSession('appId') appId: string,
+    @GetSession('userId') userId: string,
+    @Body() body: UpdateLeadDto
+  ) {
+    const lead = await this.leadService.update(appId, body);
+
+    return {
+      isSuccess: true,
+      lead,
+    };
+  }
+}
--- src/lead/lead.service.ts
+++ src/lead/lead.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ImportLeadDto } from '~/lead-list/dto/import-lead-list.dto';
 import { Lead, LeadSchemaName } from '~/mongo/campaign';
 import { ConnectionName } from '~/mongo/connections';
+import { UpdateLeadDto } from './dto/update-lead.dto';
 
 @Injectable()
 export class LeadService {
@@ -27,12 +28,43 @@ export class LeadService {
 
     const bulkOps = leads.map((lead) => ({
       updateOne: {
-        filter: { email: lead.email, leadListId: leadListId },
+        filter: {
+          email: lead.email,
+          leadListId: leadListId,
+          status: 'ACTIVE',
+        },
         update: { $set: lead },
         upsert: true,
       },
     }));
 
     await this.leadModel.bulkWrite(bulkOps);
+  }
+
+  async update(appId: string, { id, firstName, lastName }: UpdateLeadDto) {
+    const lead = await this.leadModel
+      .findOneAndUpdate(
+        {
+          _id: id,
+          appId: appId,
+          status: 'ACTIVE',
+        },
+        {
+          $set: {
+            firstName,
+            lastName,
+          },
+        },
+        {
+          new: true,
+        }
+      )
+      .lean();
+
+    if (!lead) {
+      throw new NotFoundException('LEAD_NOT_FOUND');
+    }
+
+    return lead;
   }
 }
