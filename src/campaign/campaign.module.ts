7e1ea6146e38005143ce3200f532e0e876f50768
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-02-19 16:29:41 +0000

    feat: add campaign schema

--- src/campaign/campaign.module.ts
+++ src/campaign/campaign.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CampaignSchema, CampaignSchemaName } from '~/mongo/campaign';
+import { ConnectionName } from '~/mongo/connections';
 import { CampaignController } from './campaign.controller';
 import { CampaignService } from './campaign.service';
 
+const CampaignFeature = MongooseModule.forFeature(
+  [{ name: CampaignSchemaName, schema: CampaignSchema }],
+  ConnectionName.DEFAULT
+);
+
 @Module({
+  imports: [CampaignFeature],
   controllers: [CampaignController],
   providers: [CampaignService],
 })
--- src/campaign/campaign.service.ts
+++ src/campaign/campaign.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Campaign } from '~/mongo/campaign';
 
 @Injectable()
-export class CampaignService {}
+export class CampaignService {
+  constructor(private readonly campaignModel: Model<Campaign>) {}
+}
