7558964663d9382285c15d6b5f9d8cb34d71a7aa
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-02-20 05:56:44 +0000

    feat: update campaign

--- src/campaign/campaign.service.ts
+++ src/campaign/campaign.service.ts
@@ -1,13 +1,16 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { LeadsListService } from '~/lead-list/lead-list.service';
 import { Campaign, CampaignSchemaName } from '~/mongo/campaign';
 import { ConnectionName } from '~/mongo/connections';
 import { CreateCampaignDto } from './dto/create-campaign.dto';
+import { UpdateCampaignDto } from './dto/update-campaign.dto';
 
 @Injectable()
 export class CampaignService {
   constructor(
+    private readonly leadListService: LeadsListService,
     @InjectModel(CampaignSchemaName, ConnectionName.DEFAULT)
     private readonly campaignModel: Model<Campaign>
   ) {}
@@ -17,14 +20,50 @@ export class CampaignService {
     userId: string,
     { leadListId, name, time, description }: CreateCampaignDto
   ) {
+    const leadList = await this.leadListService.getById(appId, leadListId);
+
     const campaign = await this.campaignModel.create({
       appId: appId,
       description: description,
+      leadListId: leadList._id,
       name: name,
       time: time,
-      leadListId: leadListId,
     });
 
-    return campaign;
+    return campaign.toObject();
+  }
+
+  async update(
+    appId: string,
+    userId: string,
+    { id, leadListId, ...body }: UpdateCampaignDto
+  ) {
+    const set: Partial<Campaign> = {};
+
+    Object.assign(set, body);
+
+    if (leadListId) {
+      const leadList = await this.leadListService.getById(appId, leadListId);
+      set.leadListId = leadList._id;
+    }
+
+    const campiagn = await this.campaignModel
+      .findOneAndUpdate(
+        {
+          appId: appId,
+          id: id,
+        },
+        {
+          $set: set,
+        },
+        { new: true }
+      )
+      .lean();
+
+    if (!campiagn) {
+      throw new NotFoundException('CAMPAIGN_NOT_FOUND');
+    }
+
+    return campiagn;
   }
 }
